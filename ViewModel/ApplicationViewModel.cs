6e08e3acd5274ea9f43539c54654cd49931bda81
vitnsk <121999199+vitnsk@users.noreply.github.com>
2024-03-03 14:06:54 +0700

    start

---
+++ ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_MVVM_BD_2.Model;
+
+namespace Wpf_MVVM_BD_2.ViewModel
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="iPhone 7", Company="Apple", Price=56000 },
+                new Phone {Title="Galaxy S7 Edge", Company="Samsung", Price =60000 },
+                new Phone {Title="Elite x3", Company="HP", Price=56000 },
+                new Phone {Title="Mi5S", Company="Xiaomi", Price=35000 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ViewModel/PhoneViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_MVVM_BD_2.Model;
+
+namespace Wpf_MVVM_BD_2.ViewModel
+{
+    public class PhoneViewModel : INotifyPropertyChanged
+    {
+        private Phone phone;
+
+        public PhoneViewModel(Phone p)
+        {
+            phone = p;
+        }
+
+        public string Title
+        {
+            get { return phone.Title; }
+            set
+            {
+                phone.Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string Company
+        {
+            get { return phone.Company; }
+            set
+            {
+                phone.Company = value;
+                OnPropertyChanged("Company");
+            }
+        }
+        public int Price
+        {
+            get { return phone.Price; }
+            set
+            {
+                phone.Price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
